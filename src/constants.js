01c523636b56a0a404c3998bc94f8144e0a4a068
ktan114 <ktaneight@gmail.com>
2022-07-22 10:48:26 -0500

    Add states to requesting robots function

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
 export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
+
+// This request since it is async has 3 actions
+export const REQUEST_ROBOTS_PENDING = 'REQUEST_ROBOTS_PENDING';
+export const REQUEST_ROBOTS_SUCCESS = 'REQUEST_ROBOTS_SUCCESS';
+export const REQUEST_ROBOTS_FAILED = 'REQUEST_ROBOTS_FAILED';
